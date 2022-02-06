43949b357264007fa030e61c70c06a201e9fcec7
Ana <amsanc001@gmail.com>
2022-02-05 21:02:40 -0500

    fixed errors

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,12 +1,28 @@
 
+var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
 
-var getRepoIssues = function(repo) {
-    console.log(repo);
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+  
+    if (repoName) {
+      // display repo name on the page
+      repoNameEl.textContent = repoName;
+  
+      getRepoIssues(repoName);
+    } else {
+      // if no repo was given, redirect to the homepage
+      document.location.replace("./index.html");
+    }
+};
 
+var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
@@ -18,9 +34,9 @@ var getRepoIssues = function(repo) {
                 displayWarning(repo);
               }
             });
-        }
-        else {
-          alert("There was a problem with your request!");
+        } else {
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
@@ -74,6 +90,6 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
   
-getRepoIssues("facebook/react");
+getRepoName ();
   
 
